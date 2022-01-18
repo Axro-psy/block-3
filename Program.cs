b27210d5fcd2f14a80f723fd4c0c6b064758dcf8
Axro-psy <76491158+Axro-psy@users.noreply.github.com>
2022-01-18 07:56:22 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; //чтобы не писать каждый раз Console
+namespace block_3
+{
+    class Program
+    {
+        static void Main(string[] args) //точка входа
+        {
+            Console.WriteLine("Hello, world!!!"); //вывод текста в консоль
+
+
+            ReadKey(); 
+                
+        }
+    }
+}
